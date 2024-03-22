de8de2e19277e0ce0fd4f74a5cf5431d0465b2e4
SumonaSupto <israt18@cse.pstu.ac.bd>
2024-03-22 14:29:25 +0600

    updated

--- src/component/homeElements/Testimonial.jsx
+++ src/component/homeElements/Testimonial.jsx
@@ -7,7 +7,7 @@ const Testimonial = () => {
     <div className='grid grid-cols-3 grid-rows-2 gap-4'>
 
         <div className="card w-96 bg-base-100 shadow-xl">
-        <figure><img src="https://img.freepik.com/free-photo/smiling-young-man-with-crossed-arms-outdoors_1140-255.jpg?t=st=1710925042~exp=1710928642~hmac=4f856cafd954f0f53644aafe292fd856fcf94d7451ec8c094def14ce82c72574&w=740" alt="Shoes" /></figure>
+        <figure><img src="https://img.freepik.com/free-photo/smiling-young-man-with-crossed-arms-outdoors_1140-255.jpg?t=st=1710925042~exp=1710928642~hmac=4f856cafd954f0f53644aafe292fd856fcf94d7451ec8c094def14ce82c72574&w=740" className='mx-auto w-80 h-80' alt="Shoes" /></figure>
         <div className="card-body">
        <h2 className="card-title">ALICE</h2>
        <p>I recently visited Fame Pharma for the first time, and I must say, I was impressed by the overall experience. From the moment I walked in, I was greeted by friendly staff who were ready to assist me with my needs.
@@ -17,7 +17,7 @@ The service quality at Fame Pharma was exceptional. Despite it being a busy time
       </div>
 
       <div className="card w-96 bg-base-100 shadow-xl">
-        <figure><img src="https://img.freepik.com/free-photo/cheerful-curly-business-girl-wearing-glasses_176420-206.jpg?t=st=1710924990~exp=1710928590~hmac=70f38227e71b49efeeb49d892f5881b8696166db13378439ea73cd97a49e8d75&w=996" alt="Shoes" /></figure>
+        <figure><img src="https://img.freepik.com/free-photo/cheerful-curly-business-girl-wearing-glasses_176420-206.jpg?t=st=1710924990~exp=1710928590~hmac=70f38227e71b49efeeb49d892f5881b8696166db13378439ea73cd97a49e8d75&w=996" className='mx-auto w-80 h-80' alt="Shoes" /></figure>
         <div className="card-body">
        <h2 className="card-title">REBA</h2>
        <p>I recently visited Fame Pharma for the first time, and I must say, I was impressed by the overall experience. From the moment I walked in, I was greeted by friendly staff who were ready to assist me with my needs.
@@ -27,7 +27,7 @@ The service quality at Fame Pharma was exceptional. Despite it being a busy time
       </div>
 
       <div className="card w-96 bg-base-100 shadow-xl">
-        <figure><img src="https://img.freepik.com/free-photo/close-up-young-successful-man-smiling-camera-standing-casual-outfit-against-blue-background_1258-66609.jpg?t=st=1710924893~exp=1710928493~hmac=77c712df6dc28aa70ec24cebfa85babc73b85bd7e2c1a64b295e91278426b714&w=996" alt="Shoes" /></figure>
+        <figure><img src="https://img.freepik.com/free-photo/close-up-young-successful-man-smiling-camera-standing-casual-outfit-against-blue-background_1258-66609.jpg?t=st=1710924893~exp=1710928493~hmac=77c712df6dc28aa70ec24cebfa85babc73b85bd7e2c1a64b295e91278426b714&w=996" className='mx-auto w-80 h-80' alt="Shoes" /></figure>
         <div className="card-body">
        <h2 className="card-title">RAHUL</h2>
        <p>I recently visited Fame Pharma for the first time, and I must say, I was impressed by the overall experience. From the moment I walked in, I was greeted by friendly staff who were ready to assist me with my needs.
@@ -37,7 +37,7 @@ The service quality at Fame Pharma was exceptional. Despite it being a busy time
       </div>
 
       <div className="card w-96 bg-base-100 shadow-xl">
-        <figure><img src="https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg?t=st=1710924939~exp=1710928539~hmac=9ff83a21791604c1336520b093f7c21891b78d1c5ab35cdf0d1c62e2f776410d&w=996" alt="Shoes" /></figure>
+        <figure><img src="https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg?t=st=1710924939~exp=1710928539~hmac=9ff83a21791604c1336520b093f7c21891b78d1c5ab35cdf0d1c62e2f776410d&w=996" className='mx-auto w-80 h-80'alt="Shoes" /></figure>
         <div className="card-body">
        <h2 className="card-title">BOB</h2>
        <p>I recently visited Fame Pharma for the first time, and I must say, I was impressed by the overall experience. From the moment I walked in, I was greeted by friendly staff who were ready to assist me with my needs.
